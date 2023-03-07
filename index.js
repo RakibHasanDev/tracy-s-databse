09caa4ac5abb1413f5e899c8020f5d2f90567476
“RakibHasanDev” <rakibkst16@gmail.com>
2023-03-07 06:51:43 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// console.log(process.env.STRIPE_SECRET_KEY)
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.USER_KEY}:${process.env.PASSWORD_KEY}@cluster0.h32cfqq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+
+const usersCollection = client.db('cottageHomeCareServices').collection('users');
+const messageCollection = client.db('cottageHomeCareServices').collection('messages');
+
+async function run(){
+try{
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        // console.log(user);
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    });
+
+    app.put('/users', async (req, res) => {
+        const email = req.body.email;
+        const data = req.body;
+        // console.log(data, email)
+        const query = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                name: data.name,
+                email: data.email,
+                role: data.role,
+                photoUrl: data.photoUrl,
+                verify: data.verify
+            }
+        };
+        const result = await usersCollection.updateOne(query, updateDoc, options);
+        res.send(result)
+        // console.log(email)
+        // console.log(data)
+    });
+    app.put('/messages', async (req, res) => {
+        const email = req.body.email;
+        const data = req.body;
+        // console.log(data, email)
+        const query = { email: email };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                firstName: data.firstName,
+                photoURL:data.photoURL,
+                email: data.email,
+                phone: data.phone,
+                service: data.service,
+                inquiry: data.inquiry,
+                subject: data.subject,
+                time:data.time
+            }
+        };
+        const result = await messageCollection.updateOne(query, updateDoc, options);
+        res.send(result)
+        // console.log(email)
+        // console.log(data)
+    });
+    
+
+    app.get('/users/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email }
+        const user = await usersCollection.findOne(query);
+        res.send({ isAdmin: user?.role === 'Admin' });
+    });
+
+    app.get('/allmessages/:service', async (req, res) => {
+        const service = req.params.service;
+        const query = { service: service };
+        const data = messageCollection.find(query)
+        const result = await data.toArray()
+        res.send(result);
+    });
+    app.get('/allmessages', async (req, res) => {
+        const query = {};
+        const messages = await messageCollection.find(query).toArray();
+        res.send(messages);
+    });
+   
+
+
+}
+
+finally{
+
+}
+
+}
+run().catch(console.log);
+
+app.get('/', async (req, res) => {
+    res.send('Cottage Home Care Services running');
+})
+
+app.listen(port, () => console.log(`Cottage Home Care Services Runing on ${port}`))
