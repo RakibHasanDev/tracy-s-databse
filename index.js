1aaff5af715cc574d142b117cb768a02a6d694bb
Razibul-Islam <razibulislam665@gmail.com>
2023-11-13 21:52:18 +0600

    product upload api make

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,19 +18,22 @@ app.use(express.json());
 // const uri = `mongodb+srv://${process.env.USER_KEY}:${process.env.PASSWORD_KEY}@cluster0.yk2lizo.mongodb.net/?retryWrites=true&w=majority`;
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h32cfqq.mongodb.net/?retryWrites=true&w=majority`
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h32cfqq.mongodb.net/?retryWrites=true&w=majority`
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5jjbyfi.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-console.log(uri)
+// console.log(uri)
 
-const videoCollection = client
-  .db("traceykhan")
-  .collection("videos");
+const videoCollection = client.db("traceykhan").collection("videos");
+const productCollection = client.db("traceykhan").collection("product");
 
 async function run() {
   try {
@@ -42,12 +45,12 @@ async function run() {
       const result = await videoCollection.insertOne(video);
       res.send(result);
     });
- 
+
     app.get("/videos", async (req, res) => {
       const query = {};
-      const videos= await videoCollection.find(query).toArray();
+      const videos = await videoCollection.find(query).toArray();
       res.send(videos);
-  });
+    });
 
     app.put("/users", async (req, res) => {
       const email = req.body.email;
@@ -70,7 +73,14 @@ async function run() {
       // console.log(data)
     });
 
-  
+
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+
+
   } finally {
   }
 }
